33895ae7bb7aa57cc44e59d2bf2f6baf45cf7fd8
agdielgodoyquiroz <agdielgodoyquiroz@gmail.com>
2026-05-17 13:39:43 -0400

    completed file metadata microservice

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const multer = require('multer');
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  if (!req.file) {
+    return res.json({
+      error: 'No file uploaded'
+    });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
